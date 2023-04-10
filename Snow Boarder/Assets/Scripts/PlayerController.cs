9ea1cbbcbc97a42491102ec6e69af44935227c9b
aniisahms <anisahmufida@gmail.com>
2023-04-10 18:20:21 +0700

    done

---
+++ Snow Boarder/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 20f;
+    [SerializeField] float baseSpeed = 14f;
+    [SerializeField] float slowSpeed = 8f;
+    bool canMove = true;
+
+    Rigidbody2D rb;
+    SurfaceEffector2D surfaceEfector2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        surfaceEfector2D = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    public void DisableControls()
+    {
+        canMove = false;
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEfector2D.speed = boostSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            surfaceEfector2D.speed = slowSpeed;
+        }
+        else
+        {
+            surfaceEfector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddTorque(-torqueAmount);
+        }
+    }
+}
